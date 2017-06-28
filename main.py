e22302aec6a775d79b867734cf268d4b55434c82
MichelleGrenier <mishu.grenier@gmail.com>
2017-06-28 01:02:31 -0300

    ''

--- main.py
+++
@@ -1,9 +0,0 @@
-"""Archivo principal de calculadora."""
-from calculator import parse
-
-while True:
-    try:
-        exp_str = raw_input('calc> ')
-    except EOFError:
-        break
-    print(parse(exp_str))
